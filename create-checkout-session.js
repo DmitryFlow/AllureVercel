2bca4be2dfdb5e3ef07e612e229d6cedbd45bc68
DmitryFlow <dimos548rus@gmail.com>
2024-05-18 13:07:10 +0200

    Update create-checkout-session.js

--- create-checkout-session.js
+++ create-checkout-session.js
@@ -1,23 +1,26 @@
 const stripe = require('stripe')('sk_test_51PHS7yAlI4elvZn3wZHyyXt95qSuGMOCcirsSn2wmWl2O7ZXDWAFl0JHW6ipiuv5O2rB8nLBTDTagjhj7SixuOBx000Aikat9m'); // Reemplaza con tu clave secreta de Stripe
 
 module.exports = async (req, res) => {
-    const { price } = req.body;
+    const { price, id } = req.body;
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items: [{
             price_data: {
-                currency: 'usd',
+                currency: 'eur',
                 product_data: {
                     name: 'Total Purchase',
+                    metadata: {
+                        id: id
+                    }
                 },
                 unit_amount: price * 100, // Stripe acepta centavos
             },
             quantity: 1,
         }],
         mode: 'payment',
-        success_url: 'https://tu-dominio/success', // Reemplaza con tu URL de éxito
-        cancel_url: 'https://tu-dominio/cancel',  // Reemplaza con tu URL de cancelación
+        success_url: `http://localhost:3000/summary?orderId=${id}`, // Reemplaza con tu URL de éxito
+        cancel_url: `http://localhost:3000/cancel?orderId=${id}`,  // Reemplaza con tu URL de cancelación
     });
 
     res.status(200).json({ id: session.id });
