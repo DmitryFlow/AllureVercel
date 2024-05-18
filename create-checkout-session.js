c4d7d51240343bc6458e226df692818d499778b7
DmitryFlow <dimos548rus@gmail.com>
2024-05-18 13:16:21 +0200

    Update create-checkout-session.js

--- create-checkout-session.js
+++ create-checkout-session.js
@@ -1,12 +1,16 @@
 const stripe = require('stripe')('sk_test_51PHS7yAlI4elvZn3wZHyyXt95qSuGMOCcirsSn2wmWl2O7ZXDWAFl0JHW6ipiuv5O2rB8nLBTDTagjhj7SixuOBx000Aikat9m'); // Reemplaza con tu clave secreta de Stripe
 
 module.exports = async (req, res) => {
-    const { price, id } = req.body;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests are allowed' });
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
         return;
     }
+
+    const { price, id } = req.body;
 
     try {
         const session = await stripe.checkout.sessions.create({
@@ -25,8 +29,8 @@ module.exports = async (req, res) => {
                 quantity: 1,
             }],
             mode: 'payment',
-            success_url: `http://localhost:3000/summary?orderId=${id}`, // Reemplaza con tu URL de éxito
-            cancel_url: `http://localhost:3000/summary?orderId=${id}`,  // Reemplaza con tu URL de cancelación
+            success_url: `https://tu-dominio.com/summary?orderId=${id}`, // Reemplaza con tu URL de éxito
+            cancel_url: `https://tu-dominio.com/summary?orderId=${id}`,  // Reemplaza con tu URL de cancelación
         });
 
         res.status(200).json({ id: session.id });
